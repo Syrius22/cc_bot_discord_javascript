f2c2b05f9e299364a6425f741cb012a2250fd006
Syrius22 <74452650+Syrius22@users.noreply.github.com>
2020-11-14 17:09:28 +0100

    suite bot carapuce

--- Bot.js
+++ Bot.js
@@ -1,5 +1,5 @@
-const Discord = require("discord.js");
-const bot = new Discord.client();
+const Discord = require("discord.js");  //node Bot.js pour lancer le bot
+const bot = new Discord.Client();
 bot.on("ready", function() {
 	console.log("carapuce est dans les places !");
 });
@@ -10,4 +10,44 @@ bot.on("message",message=> {
 	if (message.content === "!ping") {
 		message.channel.send("Carapong !");
 	}
+	if (message.content === "!carabonjour") {
+		message.reply("carabonjour à toi !");
+	}
+	if (message.content.startsWith("!pin")) {
+		message.pin();
+	}
+	if (message.content === "!emojiliste") 
+	{				
+		const emojiliste = message.guild.emojis.cache.map((e) => e);
+		message.channel.send(emojiliste);
+	}
+	if (message.content === "!carahelp") {
+	//on envoie un message de type embed dans le channel d'où
+	//provient le message
+		message.channel.send({
+			embed: {
+				color: 3447003,
+				description: "__**Les différentes commandes**__",
+				fields: [
+					{
+						name: "!carahelp",
+						value: "Pour afficher cette aide."
+					},
+					{
+						name: "!ping",
+						value: "Pong !"
+					},
+					{
+						name: "!pin",
+						value: "Pour épingler un message"
+					},
+					{
+						name:"!emojiliste",
+						value:"Pour connaître toutes les emojis du serveur"
+					},
+				]
+			}
+		});
+	}
+
 });
